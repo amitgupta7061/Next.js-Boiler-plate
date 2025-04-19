c4a22368e6ebf1324b16da6aa19b21ef8c6e2aa2
Amit <amitgupta561710@gmail.com>
2025-04-19 15:45:03 +0530

    initial

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-red-500 font-bold text-center w h-full'>
+      Hello world
+    </div>
+  )
+}
+
+export default page
